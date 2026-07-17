c1fa77e9fca6299a562a6c223579740412a58cd2
Vaishnavi1325 <vaishnavishukla9578b@gmail.com>
2026-07-17 14:27:38 +0530

    Add transaction ledger feature

---
+++ src/models/ledger.model.js
@@ -0,0 +1,46 @@
+const mongoose=require('mongoose')
+
+const ledgerSchema=new Mongoose.Schema({
+    account:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        index:true,
+        required:[true,"Ledger must be associated with an account"],
+        immutable:true
+    },
+    amount:{
+        type:Number,
+        required:[true,"Amount is required for creating a ledger"]
+    },
+    transaction:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        index:true,
+        required:[true,"Ledger must be associated with a transaction"],
+        immutable:true
+    },
+    type:{
+        type:String,
+        enum:{value:["CREDIT","DEBIT"],message:"Type must be either credit or debit"},
+        required:[true,"Ledger tyoe is required"],
+        immutable:true
+    }
+    
+})
+function preventLedgerModification() {
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted");
+}
+
+ledgerSchema.pre('findOneAndUpdate', preventLedgerModification);
+ledgerSchema.pre('updateOne', preventLedgerModification);
+ledgerSchema.pre('deleteOne', preventLedgerModification);
+ledgerSchema.pre('remove', preventLedgerModification);
+ledgerSchema.pre('deleteMany', preventLedgerModification);
+ledgerSchema.pre('updateMany', preventLedgerModification);
+ledgerSchema.pre("findOneAndDelete", preventLedgerModification);
+ledgerSchema.pre("findOneAndReplace", preventLedgerModification);
+
+
+const ledgerModel = mongoose.model('ledger', ledgerSchema);
+
+module.exports = ledgerModel;
